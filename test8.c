d64e548fcaf098e162de8ee22217e41f26449a5f
Spectralpt <miguelfranquiralves@gmail.com>
2023-04-29 02:20:34 +0100

    minor changes

--- test8.c
+++ test8.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define GRID_SIZE 20;
+
 typedef struct polygon{
   Vector2 vertices[4];
 }Polygon ;
@@ -69,13 +71,13 @@ bool polyPoint(Polygon polygon, float pointX, float pointY) {
   return collision;
 }
 
-void drawCube(Texture2D *cubes , int frames, int **heightMutationMap, int **map){
+void drawCube(Texture2D *cubes , int frames, double **heightMutationMap, int **map){
 
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  int mapHeight = 9;
-  int mapWidth = 9;
+  int mapHeight = 20;
+  int mapWidth = 20;
 
   int tileScaleFactor = 2;
   
@@ -133,47 +135,47 @@ int** randomMap(int mapWidth, int mapHeight){
   return map;
 }
 
-int** generateMap() {
+int** generateMap(int width, int height) {
     int **map;
     int i, j, k, l;
     int num_water_tiles = 0;
-    int size = 9;
+    int size = width > height ? width : height;
     int max_water_tiles = size * size / 4;
 
     // seed the random number generator
     srand(time(NULL));
 
     // allocate memory for map
-    map = (int**)malloc(size*sizeof(int*));
-    for (i = 0; i < size; i++) {
-        map[i] = (int*)malloc(size*sizeof(int));
+    map = (int**)malloc(height*sizeof(int*));
+    for (i = 0; i < height; i++) {
+        map[i] = (int*)malloc(width*sizeof(int));
     }
 
     // initialize map to all dirt
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
             map[i][j] = Grass;
         }
     }
 
     // add some random water tiles
     for (k = 0; k < 3; k++) {
-        i = rand() % size;
-        j = rand() % size;
+        i = rand() % height;
+        j = rand() % width;
         map[i][j] = Water;
         num_water_tiles++;
     }
 
     // add some more water tiles by spreading out from existing ones
     while (num_water_tiles < max_water_tiles) {
-        for (i = 0; i < size; i++) {
-            for (j = 0; j < size; j++) {
+        for (i = 0; i < height; i++) {
+            for (j = 0; j < width; j++) {
                 if (map[i][j] == Water) {
                     if (i > 0 && map[i-1][j] != Water && rand() % 2 == 0) {
                         map[i-1][j] = Water;
                         num_water_tiles++;
                     }
-                    if (i < size-1 && map[i+1][j] != Water && rand() % 2 == 0) {
+                    if (i < height-1 && map[i+1][j] != Water && rand() % 2 == 0) {
                         map[i+1][j] = Water;
                         num_water_tiles++;
                     }
@@ -181,7 +183,7 @@ int** generateMap() {
                         map[i][j-1] = Water;
                         num_water_tiles++;
                     }
-                    if (j < size-1 && map[i][j+1] != Water && rand() % 2 == 0) {
+                    if (j < width-1 && map[i][j+1] != Water && rand() % 2 == 0) {
                         map[i][j+1] = Water;
                         num_water_tiles++;
                     }
@@ -191,19 +193,19 @@ int** generateMap() {
     }
 
     // add sand around water
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
             if (map[i][j] == Water) {
                 if (i > 0 && map[i-1][j] != Water) {
                     map[i-1][j] = Sand;
                 }
-                if (i < size-1 && map[i+1][j] != Water) {
+                if (i < height-1 && map[i+1][j] != Water) {
                     map[i+1][j] = Sand;
                 }
                 if (j > 0 && map[i][j-1] != Water) {
                     map[i][j-1] = Sand;
                 }
-                if (j < size-1 && map[i][j+1] != Water) {
+                if (j < width-1 && map[i][j+1] != Water) {
                     map[i][j+1] = Sand;
                 }
             }
@@ -219,13 +221,110 @@ int** heightMutator(int mapWidth, int mapHeight){
     heightMutationMap[i] = (int*)malloc(mapHeight * sizeof(int));
   }
 
-  for(int i = 0; i < 9; i++){
-      for(int j = 0; j < 9; j++){
+  for(int i = 0; i < mapWidth; i++){
+      for(int j = 0; j < mapHeight; j++){
           heightMutationMap[i][j] = rand() % 4;
       }
   }
   return heightMutationMap;
 }
+
+double **generatePerlinNoise(int width, int height, double frequency, int octaves, double persistence)
+{
+    // allocate memory for the 2D array
+    double **noise = (double**)malloc(height * sizeof(double*));
+    for (int i = 0; i < height; i++) {
+        noise[i] = (double*)malloc(width * sizeof(double));
+    }
+
+    // generate the noise grid
+    double amplitude = 1.0;
+    double total_amplitude = 0.0;
+    for (int octave = 0; octave < octaves; octave++) {
+        // generate a new grid for this octave
+        double **grid = (double**)malloc((width+1) * sizeof(double*));
+        for (int i = 0; i < width+1; i++) {
+            grid[i] = (double*)malloc((height+1) * sizeof(double));
+        }
+        for (int i = 0; i < width+1; i++) {
+            for (int j = 0; j < height+1; j++) {
+                grid[i][j] = (double)rand() / RAND_MAX;
+            }
+        }
+
+        // interpolate the grid to the target size
+        int step_size = width / width;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                double x = j * step_size * frequency;
+                double y = i * step_size * frequency;
+
+                int x0 = floor(x);
+                int y0 = floor(y);
+                int x1 = x0 + 1;
+                int y1 = y0 + 1;
+
+                double u = x - x0;
+                double v = y - y0;
+
+                double e00 = grid[x0][y0];
+                double e01 = grid[x0][y1];
+                double e10 = grid[x1][y0];
+                double e11 = grid[x1][y1];
+
+                double a = e00;
+                double b = e10 - e00;
+                double c = e01 - e00;
+                double d = e00 - e10 - e01 + e11;
+
+                noise[i][j] += (a + b*u + c*v + d*u*v) * amplitude;
+            }
+        }
+
+        // update the frequency and amplitude for the next octave
+        frequency *= 2;
+        total_amplitude += amplitude;
+        amplitude *= persistence;
+
+        // free the grid memory
+        for (int i = 0; i < width+1; i++) {
+            free(grid[i]);
+        }
+        free(grid);
+    }
+
+
+    // // normalize the noise values to be between 0 and 1
+    // for (int i = 0; i < height; i++) {
+    //     for (int j = 0; j < width; j++) {
+    //         noise[i][j] /= total_amplitude;
+    //         noise[i][j] = fmax(0, noise[i][j]);
+    //     }
+    // }
+
+for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            noise[i][j] /= total_amplitude;
+            noise[i][j] *= 5.0;
+            noise[i][j] = round(noise[i][j]);
+            noise[i][j] = fmax(0.0, noise[i][j]);
+        }
+    }
+    // round the noise values to the nearest integer
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                noise[i][j] = round(noise[i][j]);
+            }
+        }
+    for (int i = 0; i < height; i++) {
+        printf("\n");
+        for (int j = 0; j < width; j++) {
+          printf("%lf", noise[i][j]);
+        }
+    }
+    return noise;
+}
+
 
 Texture2D* textureLoader(){
 
@@ -264,14 +363,15 @@ int main(void)
   SetTargetFPS(60);
   int framesCounter = 0;
 
-  int mapWidth = 9, mapHeight = 9;
-  int **heightMutationMap = heightMutator(mapWidth, mapHeight);
+  int mapWidth = 20, mapHeight = 20;
+  // int **heightMutationMap = heightMutator(mapWidth, mapHeight);
+  double **heightMutationMap = generatePerlinNoise(mapWidth, mapHeight, 0.05, 5, 2);
 
   Texture2D *textures = (Texture2D*)malloc(3 * sizeof(Texture2D));
   textures = textureLoader();
 
   // int **map = randomMap(mapWidth, mapHeight);
-  int **map = generateMap();
+  int **map = generateMap(mapWidth, mapHeight);
 
   while (!WindowShouldClose()) {
     ClearBackground((Color){34, 32, 52, 255});
